30cb997317b8f859e00b3ca05947f84dc1a9aeac
Eric Estabrooks <eestabrooks@datakitchen.io>
2016-08-18 17:23:03 -0400

    load the AWS keys

--- start_vault_dev.sh
+++ start_vault_dev.sh
@@ -3,6 +3,8 @@
 # Put vault executable into the location that you want
 # Edit /etc/environment to include path to vault
 # Define your AWS Keys in the environment: ACCESS_KEY and SECRET_KEY
+source secret.sh
+
 nohup vault server -dev &
 export VAULT_ADDR='http://127.0.0.1:8200'
 vault mount aws
